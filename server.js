25357b76f873a06f9fc41cae49bbc61ee9a6b0ac
a20j30 <aida.jevric@gmail.com>
2022-05-15 15:01:39 -0400

    cors fixes

--- server.js
+++ server.js
@@ -4,21 +4,36 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
 const { notFound, errorHandler } = require('./middlewares/errorMiddleware');
-
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 dotenv.config();
 connectDB();
 
 app.use(express.json());
 
+app.use((req, res, next)=>{
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
+
+app.get('api/users/allusers',(req, res)=>{
+    request(
+        {url:'https://web-reads-backend.herokuapp.com/api/users/allusers'},
+        (error, response, body)=>{
+            if (error || response.statusCode !== 200){
+                return res.status(500).json({type: 'error', message: err.message});
+            }
+            res.json(JSON.parse(body));
+        }
+    )
+});
+
 //Api GET request
 app.get("/", (req,res) => {
     res.send("API is running for Webreads");
 });
-
-
-
 
 // using the userRoutes file from the routes folder
 app.use('/api/users', userRoutes) 
